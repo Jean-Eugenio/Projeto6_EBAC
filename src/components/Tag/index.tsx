f11660241587c5402fbf72607ef7a3db0dd8dbc7
Jean-Eugenio <jeaneugeniodev2024@gmail.com>
2024-11-15 14:02:02 -0300

    Delete src directory

--- src/components/Tag/index.tsx
+++
@@ -1,9 +0,0 @@
-import { TagCategoria } from './styles'
-
-export type Props = {
-  children: string
-}
-
-const Tag = ({ children }: Props) => <TagCategoria>{children}</TagCategoria>
-
-export default Tag
